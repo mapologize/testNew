30ab33c9777c3ce2315054c4a0ae47bf0c34e43d
mapologize <polycashpro@gmail.com>
2023-11-14 11:58:32 +0700

    EEE

--- src/api.js
+++ src/api.js
@@ -17,6 +17,7 @@ router.get('/mooc2/metadata/:tokenid', (req,res) => {
     const mod = tokenid % 1000;
     const infoId = parseInt(tokenid) + parseInt(1);
     const infoImage = parseInt(mod) + parseInt(1);
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.json({
         "image": 'ipfs://QmSfV3X1KUtpjDaz7ruWrCQmxjvuncWb5wdaPHDQZtBBRf/' + infoImage + '.png',
