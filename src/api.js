fb0f24382461a2600e68be15016bea647ad43029
Penzer <dedonoiizunfinity@gmail.com>
2023-12-14 08:38:13 +0700

    main

--- src/api.js
+++ src/api.js
@@ -12,6 +12,35 @@ router.get('/', (req,res) => {
     });
 });
 
+router.get('/mooc1/metadata/:tokenid', (req,res) => {
+  const tokenid = req.params.tokenid;
+  const mod = tokenid % 1000;
+  const infoId = parseInt(tokenid) + parseInt(1);
+  const infoImage = parseInt(mod) + parseInt(1);
+  let ipfsPart = "";
+  if(tokenid<1801){
+    ipfsPart = "QmSW2fMVyjrfxRQTjSDg1ktHJpGYppsNe5QE3VNaCmqgVj";
+  }else{
+    ipfsPart = "QmduWNQnZH3bff9DWxtZmMuxj7HYpXPx1BWUUz9ek6ks3a";
+  }
+  res.setHeader('Access-Control-Allow-Origin', 'https://moomooh.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.json({
+      "image": 'ipfs://'+ ipfsPart +'/' + infoImage + '.png',
+      "name": 'MooMooh NFT #' + infoId,
+      "external_url": "",
+      "description": "Playing Oracle NFT with MooMooh.io",
+      "attributes": [
+        {
+          "trait_type": "Website",
+          "value": "moomooh.io"
+        }
+      ],
+      "unlockable_content": [],
+      "explicit_and_sensitive_content": false
+  });
+});
+
 router.get('/mooc2/metadata/:tokenid', (req,res) => {
     const tokenid = req.params.tokenid;
     const mod = tokenid % 1000;
